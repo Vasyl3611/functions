222a8d7074f7b2ff086968d08a129d18c8da659f
Vasyl3611 <30364013+Vasyl3611@users.noreply.github.com>
2020-04-09 10:15:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+class User {
+    constructor(name, surname) {
+        this.name = name;
+        this.surname = surname;
+        this.eventList = [];
+    }
+    getName() {
+        return this.name + " " + this.surname;
+    }
+    getUserInfo() {
+        console.log(this.name + " " + this.surname);
+    }
+    addEventobject(eventObject) {
+        this.eventList.push(eventObject);
+    }
+}
+
+class Event {
+    constructor(name, location, price) {
+        this.name = name;
+        this.location = location;
+        this.price = price;
+    }
+    getName() {
+        return this.name;
+    }
+    setName() {
+        this.name = eventName;
+    }
+}
